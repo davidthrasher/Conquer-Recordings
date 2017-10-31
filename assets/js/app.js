11cef9892da868ebda5a348e120b00e5b04e3f5b
David Thrasher <thrasher1332@hotmail.com>
2017-10-30 21:23:06 -0400

    Added Images in gear section and updated about section

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,13 @@
         $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
     });
 
+    $(".links-gear").on('click', function(){
+        //jQuery function which triggers when developers link clicked
+        $("#gear").css("display", "block");       //displays the previously hidden container class block
+        lastElementTop = $('#gearsection').position().top - 150 ; //finds the position of the container class and assigns to variable
+        $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
+    });
+
     $(".links-contact").on('click', function(){
         //jQuery function which triggers when developers link clicked
         $("#contact").css("display", "block");       //displays the previously hidden container class block
